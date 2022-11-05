20ee0ad1242853f687b6692e6bb4dd62b80aef50
akihiro <aponmaru@gmail.com>
2022-11-06 07:03:29 +0900

    [add] item 追加

--- src/consts/Item.ts
+++ src/consts/Item.ts
@@ -56,6 +56,42 @@ const items = [
       'あお',
     ],
   },
+  {
+    title: 'はやぶさ',
+    description: '',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/JRE-TEC-E5_omiya.JPG/2880px-JRE-TEC-E5_omiya.JPG',
+    tags: [
+      'みどり',
+      'しんかんせん',
+    ],
+  },
+  {
+    title: 'こまち',
+    description: '',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Series-E6_Z14_Komachi.jpg/2880px-Series-E6_Z14_Komachi.jpg',
+    tags: [
+      'あか',
+      'しんかんせん',
+    ],
+  },
+  {
+    title: 'こだま',
+    description: '',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/JRW-500_V2_inHimeji.jpg/2880px-JRW-500_V2_inHimeji.jpg',
+    tags: [
+      'あお',
+      'しんかんせん',
+    ],
+  },
+  {
+    title: 'ゆふいんの森',
+    description: '',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Yufuin-no-Mori-72.jpg/2880px-Yufuin-no-Mori-72.jpg',
+    tags: [
+      'みどり',
+      'とっきゅう',
+    ],
+  },
   // {
     // title: '',
     // description: '',
